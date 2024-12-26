dab2a6f8056f6e4ecab5b1b4c05ceefd621cb3ae
kawkaw5000 <lexeramorcillo@gmail.com>
2024-12-26 23:29:38 +0800

    Added Params and Comments Practice

--- backend/src/controllers/baseController.ts
+++ backend/src/controllers/baseController.ts
@@ -1,20 +1,42 @@
 import { Response, Request } from 'express';
 import { UserManager } from '../services/userManager';
 
+/**
+ * A string to store error messages.
+ * An instance of the UserManager class to handle user-related operations.
+ */
 export class BaseController {
   public errorMessage: string;
   public userMgr: UserManager;
 
+  /**
+   * Constructor to initialize the BaseController.
+   * Sets up the errorMessage to an empty string and creates a new instance of UserManager.
+   */
   constructor() {
     this.errorMessage = '';
     this.userMgr = new UserManager();
   }
 
+  /**
+   * Sends an error response with a given message.
+   * 
+   * @param res - The response object from Express to send the error response.
+   * @param message - The error message to send in the response body.
+   * @returns {Response} - The Express response object with a 400 status code and the error message.
+   */
   public sendError(res: Response, message: string) {
     this.errorMessage = message;
     return res.status(400).json({ error: this.errorMessage });
   }
 
+  /**
+   * Sends a success response with the provided data.
+   * 
+   * @param res - The response object from Express to send the success response.
+   * @param data - The data to be included in the response body.
+   * @returns {Response} - The Express response object with a 200 status code and the data.
+   */
   public sendSuccess(res: Response, data: any) {
     return res.status(200).json({ data });
   }
--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,18 +3,23 @@ import { BaseController } from './baseController';
 import { ErrorCode } from '../utils/constants';
 
 export class UserController extends BaseController {
+  /**
+   * Constructor to initialize the UserController.
+   * Binds the Register and Login methods to the controller instance.
+   */
   constructor() {
     super();
     this.Register = this.Register.bind(this);
     this.Login = this.Login.bind(this);
   }
 
-  /// <summary>
-  /// Registers a new user and creates an account.
-  /// </summary>
-  /// <param name="req">The request object.</param>
-  /// <param name="res">The response object.</param>
-  /// <returns> A response indicating success or failure </returns>
+  /**
+   * Registers a new user by creating an account with the provided username and password.
+   * 
+   * @param req - The request object containing the user's data in the body (username and password).
+   * @param res - The response object used to send the success or error response.
+   * @returns {Promise<Response>} - The response object containing either the success message or an error message.
+   */
   public async Register(req: Request, res: Response): Promise<Response> {
     const { Username, Password } = req.body;
 
@@ -31,12 +36,13 @@ export class UserController extends BaseController {
     }
   }
 
-  /// <summary>
-  /// Authenticate user and signs the user in when successful.
-  /// </summary>
-  /// <param name="req">The request object.</param>
-  /// <param name="res">The response object.</param>
-  /// <returns> A response with a token or an error message </returns>
+  /**
+   * Authenticates a user by verifying the provided username and password, and returns a JWT token if successful.
+   * 
+   * @param req - The request object containing the user's login credentials (username and password).
+   * @param res - The response object used to send the success or error response.
+   * @returns {Promise<Response>} - The response object containing either the success message and token or an error message.
+   */
   public async Login(req: Request, res: Response): Promise<Response> {
     const { Username, Password } = req.body;
 
